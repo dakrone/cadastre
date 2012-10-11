e752648180c9303f741011e15c0e197bd3df8004
Lee Hinman <lee@writequit.org>
2012-10-11 15:52:07 -0600

    empty project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cadastre
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cadastre/core_test.clj
@@ -0,0 +1,7 @@
+(ns cadastre.core-test
+  (:use clojure.test
+        cadastre.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
