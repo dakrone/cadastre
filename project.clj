8179b929ac30ea1ad4c0785195b0337095411717
Lee Hinman <lee@writequit.org>
2012-10-11 16:39:21 -0600

    clojure as an optional dep

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/dakrone/cadastre"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.namespace "0.2.0"]
-                 [cheshire "4.0.3"]])
+  :dependencies [[org.clojure/tools.namespace "0.2.0"]
+                 [cheshire "4.0.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}})
